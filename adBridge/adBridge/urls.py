2ca9b8421f25700920a8ecc1a37debc8113b2e53
rupesh-ps <182068544+rupesh-ps@users.noreply.github.com>
2024-11-02 14:35:44 +0000

    Add more information in homepage and image in ads

--- adBridge/adBridge/urls.py
+++ adBridge/adBridge/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from ads import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path("ads/", views.AdListView.as_view(), name="ad-list"),
     path('ad/<int:pk>/', views.AdDetailView.as_view(), name='ad-detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- adBridge/ads/admin.py
+++ adBridge/ads/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Category, Ad, Message
+from .models import Profile, Category, Ad, Message, AdImages
 
 admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Ad)
 admin.site.register(Message)
+admin.site.register(AdImages)
