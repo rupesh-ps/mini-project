b7a8c692f28f38e2a5c05917373f54ee4899c522
rupesh-ps <182068544+rupesh-ps@users.noreply.github.com>
2024-11-05 12:57:50 +0000

    Implement authentication flow for login/logout

--- adBridge/adBridge/settings.py
+++ adBridge/adBridge/settings.py
@@ -130,3 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'homepage'
+
+LOGOUT_REDIRECT_URL = 'homepage'
--- adBridge/adBridge/urls.py
+++ adBridge/adBridge/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from ads import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePageView.as_view(), name='homepage'),
     path("signup/", accounts_views.signup, name="signup"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("login/", auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('category/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
     path("ads/", views.AdListView.as_view(), name="ad-list"),
