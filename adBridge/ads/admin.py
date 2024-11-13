0c0c932f88c2c291490d966edb333d75d0640872
rupesh-ps <182068544+rupesh-ps@users.noreply.github.com>
2024-11-13 05:46:55 +0000

    Deleted redundant models from `ads` app
    As new app `ad_chat` is created and its models -
    `Chat` and `ChatMessage`, the old `Message` model
    from `ads` app is removed and deleted from database.

--- adBridge/ads/admin.py
+++ adBridge/ads/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Category, Ad, Message, AdImages
+from .models import Profile, Category, Ad, AdImages
 
 admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Ad)
-admin.site.register(Message)
 admin.site.register(AdImages)
